f88d83e9008d301295b34ecfb2304930db705b63
Jason Martocci <martoccijason@gmail.com>
2016-04-20 10:56:01 -0400

    ride the bus

--- bus.js
+++ bus.js
@@ -1,29 +1,43 @@
 var Student = require('./student.js');
-
-// var fs = require('fs');
+var Main = require('./main.js');
 
 var Bus = function() {
-  this.studentsOnTheBus = [];
-  this.driverName = "";
-  this.color = "";
-  this.gas = "";
+    this.studentsOnTheBus = [];
+    this.driverName = "";
+    this.color = "";
+    this.gas = "";
 
-  this.studentEntersBus = function(name, gender, grade, gpa, detentions, sleepingInClass, catchPhrase) {
-    console.log("Student Enters Bus");
-  };
+    this.studentEntersBus = function(name, gender, grade, GPA, detentions, sleepingInClass, catchPhrase){
 
-  this.busChatter = function(detentions, gpa, catchPhrase) {
-    if((detentions < 10) && (gpa > 2)) {
-      console.log(catchPhrase);
-    } else {
-      console.log("Student still cannot have fun");
-    }
+    this.studentsOnTheBus.push(new Student(name, gender, grade, GPA, detentions, sleepingInClass, catchPhrase))
+
+    this.busChatter = function(detentions, gpa, catchPhrase) {
+      if((detentions < 10) && (gpa > 2)) {
+        console.log(catchPhrase);
+      } else {
+        console.log("Student still cannot have fun");
+      }
+    };
   };
 };
-// var busCreator = new Bus(result.driverName, result.color, result.gas);    
-//   fs.appendFile('busLog.txt', JSON.stringify(busCreator) + '\r\n', 'utf8', function(error){
-//     if(error) throw error
-//     else{console.log("Bus created");}
-//   });
+
+var rideTheBus = new Bus();
+
+rideTheBus.studentEntersBus('Janet', 'female', '12', '3.5', 'No', 'Yes', 'Hello World!');
+rideTheBus.studentEntersBus('Steve', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
+rideTheBus.studentEntersBus('Kevin', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
+rideTheBus.studentEntersBus('Matthew', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
+rideTheBus.studentEntersBus('Kaitlyn', 'female', '12', '3.5', 'No', 'Yes', 'Hello World!');
+rideTheBus.studentEntersBus('Mark', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
+rideTheBus.studentEntersBus('Mary', 'female', '12', '3.5', 'No', 'Yes', 'Hello World!');
+rideTheBus.studentEntersBus('Lora', 'female', '12', '3.5', 'No', 'Yes', 'Hello World!');
+rideTheBus.studentEntersBus('Jeff', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
+rideTheBus.studentEntersBus('Chris', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
+
+for (var studentsOnTheBus = 0; studentsOnTheBus < 10; studentsOnTheBus++) {
+  console.log(rideTheBus.studentsOnTheBus[studentsOnTheBus].name);
+}
+
+// Bus.busChatter();
 
 module.exports = Bus;
--- main.js
+++ main.js
@@ -2,25 +2,14 @@ var Bus = require('./bus.js');
 var Student = require('./student.js');
 var prompt = require('prompt');
 
-var school = new Bus(studentOne);
-
-var studentOne = new Student('Janet', 'female', '12', '3.5', 'No', 'Yes', 'Hello World!');
-var studentTwo = new Student('Steve', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
-var studentThree = new Student('Kevin', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
-var studentFour = new Student('Matthew', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
-var studentFive = new Student('Kaitlyn', 'female', '12', '3.5', 'No', 'Yes', 'Hello World!');
-var studentSix = new Student('Mark', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
-var studentSeven = new Student('Mary', 'female', '12', '3.5', 'No', 'Yes', 'Hello World!');
-var studentEight = new Student('Lora', 'female', '12', '3.5', 'No', 'Yes', 'Hello World!');
-var studentNine = new Student('Jeff', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
-var studentTen = new Student('Chris', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
+var school = new Bus();
 
 prompt.get(['name', 'gender', 'grade', 'gpa', 'detentions', 'sleepingInClass', 'catchPhrase'], function(err, result){
-  var s = new Student(result.name, result.gender, result.grade, result.gpa, result.detentions, result.sleepingInClass, result.catchPhrase);
+	var s = new Student(result.name, result.gender, result.grade, result.gpa, result.detentions, result.sleepingInClass, result.catchPhrase);
 
-  school.studentEntersBus(result.name, result.gender, result.grade, result.gpa, result.detentions, result.sleepingInClass, result.catchPhrase);
+	s.canStudentHaveFun();
 
-  s.canStudentHaveFun();
+	school.studentEntersBus(result.name, result.gender, result.grade, result.gpa, result.detentions, result.sleepingInClass, result.catchPhrase);
 
-  school.busChatter(result.gpa, result.detentions, result.catchPhrase);
+	school.busChatter(result.gpa, result.detentions, result.catchPhrase);
 });
