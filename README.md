f3021ac38774071564206eb9fc8afb945835024e
Jason Martocci <martoccijason@gmail.com>
2016-04-20 11:29:03 -0400

    the bus is rolling

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# ConstructorFunctions
+Week 11 -- 'The Bus'
+This is strictly a homework exercise on Constructor Functions.
+
+After you run 'node main.js', you'll see a series of prompts containing a 'student info' questionaire. Fill out the prompts and the program will display if you can have fun or not.
+
+Jason
--- bus.js
+++ bus.js
@@ -17,27 +17,30 @@ var Bus = function() {
       } else {
         console.log("Student still cannot have fun");
       }
+      var rideTheBus = new Bus();
+
+      rideTheBus.studentEntersBus('Janet', 'female', '12', '1', '2', 'Yes', 'Weeeeeeee');
+      rideTheBus.studentEntersBus('Steve', 'male', '12', '2.5', '2', 'Yes', 'I Love Computers');
+      rideTheBus.studentEntersBus('Kevin', 'male', '12', '3.5', '2', 'Yes', 'Be kind to geeks');
+      rideTheBus.studentEntersBus('Matthew', 'male', '12', '1.5', '2', 'Yes', 'Video games save the world.');
+      rideTheBus.studentEntersBus('Kaitlyn', 'female', '12', '3.5', '2', 'Yes', 'I love to read.');
+      rideTheBus.studentEntersBus('Mark', 'male', '12', '4.5', '2', 'Yes', 'Hello World!');
+      rideTheBus.studentEntersBus('Mary', 'female', '12', '1.5', '2', 'Yes', 'What are you doing?');
+      rideTheBus.studentEntersBus('Lora', 'female', '12', '3.5', '2', 'Yes', 'Work Work Work Work...');
+      rideTheBus.studentEntersBus('Jeff', 'male', '12', '3.2', '2', 'Yes', 'Lets get it on.');
+      rideTheBus.studentEntersBus('Chris', 'male', '12', '2.5', '2', 'Yes', 'Good Bye');
+
+      for (var studentsOnTheBus = 0; studentsOnTheBus < 10; studentsOnTheBus++) {
+        console.log("-----------------------------------");
+
+        if((rideTheBus.studentsOnTheBus[studentsOnTheBus].detentions < 10) && (rideTheBus.studentsOnTheBus[studentsOnTheBus].gpa > 2)) {
+          console.log(rideTheBus.studentsOnTheBus[studentsOnTheBus].name + " " + rideTheBus.studentsOnTheBus[studentsOnTheBus].catchPhrase);
+          } else {
+          console.log(rideTheBus.studentsOnTheBus[studentsOnTheBus].name + " " + "still cannot have fun");
+        }
+      }
     };
   };
 };
-
-var rideTheBus = new Bus();
-
-rideTheBus.studentEntersBus('Janet', 'female', '12', '3.5', 'No', 'Yes', 'Hello World!');
-rideTheBus.studentEntersBus('Steve', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
-rideTheBus.studentEntersBus('Kevin', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
-rideTheBus.studentEntersBus('Matthew', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
-rideTheBus.studentEntersBus('Kaitlyn', 'female', '12', '3.5', 'No', 'Yes', 'Hello World!');
-rideTheBus.studentEntersBus('Mark', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
-rideTheBus.studentEntersBus('Mary', 'female', '12', '3.5', 'No', 'Yes', 'Hello World!');
-rideTheBus.studentEntersBus('Lora', 'female', '12', '3.5', 'No', 'Yes', 'Hello World!');
-rideTheBus.studentEntersBus('Jeff', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
-rideTheBus.studentEntersBus('Chris', 'male', '12', '3.5', 'No', 'Yes', 'Hello World!');
-
-for (var studentsOnTheBus = 0; studentsOnTheBus < 10; studentsOnTheBus++) {
-  console.log(rideTheBus.studentsOnTheBus[studentsOnTheBus].name);
-}
-
-// Bus.busChatter();
 
 module.exports = Bus;
